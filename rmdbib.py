7a6896922f33cfc1d4da1424e1fc5283e47d74c6
zhuth <zthpublic@gmail.com>
2016-02-07 15:54:32 +0800

    use lxml

--- rmdbib.py
+++ rmdbib.py
@@ -85,12 +85,87 @@ def docx_deal(docx):
     for _ in z.namelist():
         zf[_] = z.read(_)
     z.close()
+    
+    import lxml.etree as ET
+    import copy
+    
+    nss = {
+        'w':"http://schemas.openxmlformats.org/wordprocessingml/2006/main",
+        'm':"http://schemas.openxmlformats.org/officeDocument/2006/math",
+        'r':"http://schemas.openxmlformats.org/officeDocument/2006/relationships",
+        'o':"urn:schemas-microsoft-com:office:office",
+        'v':"urn:schemas-microsoft-com:vml",
+        'w10':"urn:schemas-microsoft-com:office:word",
+        'a':"http://schemas.openxmlformats.org/drawingml/2006/main",
+        'pic':"http://schemas.openxmlformats.org/drawingml/2006/picture",
+        'wp':"http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"
+    }
 
     doc = zf['word/document.xml'].decode('utf-8')
-    doc = re.sub(ur'([…·“”‘’])', u'</w:t></w:r><w:r><w:rPr><w:rFonts w:asciiTheme="majorEastAsia" w:eastAsiaTheme="majorEastAsia" w:hAnsiTheme="majorEastAsia"/></w:rPr><w:t>\\1</w:t></w:r><w:r><w:t>', doc)
-    doc = re.sub(ur'<w:p>(<w:pPr><w:pStyle w:val="(FirstParagraph|BodyText)" />)', u'<w:p>\\1<w:ind w:firstLineChars="200" w:firstLine="420"/>', doc)
-    # print doc
-    zf['word/document.xml'] = doc.encode('utf-8')
+    #doc = re.sub(ur'([…·“”‘’])', u'</w:t></w:r><w:r><w:rPr><w:rFonts w:asciiTheme="majorEastAsia" w:eastAsiaTheme="majorEastAsia" w:hAnsiTheme="majorEastAsia"/></w:rPr><w:t>\\1</w:t></w:r><w:r><w:t>', doc)
+    #doc = re.sub(ur'<w:p>(<w:pPr><w:pStyle w:val="(FirstParagraph|BodyText)" />)', u'<w:p>\\1<w:ind w:firstLineChars="200" w:firstLine="420"/>', doc)
+    doc = doc.encode('utf-8')
+
+    #doc = zf['word/document.xml']
+    xt = ET.fromstring(doc)
+    
+    def _deal_p(_p):
+        ps = _p.xpath('.//w:pStyle', namespaces=nss)
+        if len(ps) == 0 or ps[0].attrib.get('{%s}val' % nss['w'], 'BodyText') in ['BodyText', 'FirstParagraph', '']:
+            ppr = _p.xpath('./w:pPr', namespaces=nss)
+            if len(ppr) == 0:
+                ppr = ET.Element('{%s}pPr' % nss['w'], nsmap=nss)
+                _p.insert(0, ppr)
+            else:
+                ppr = ppr[0]
+                
+            ppr.append(ET.Element('{%s}ind' % nss['w'], {
+                '{%s}firstLineChars' % nss['w']: "200",
+                '{%s}firstLine'      % nss['w']: "420"
+            }, nsmap=nss))
+    
+    def _deal_t(_t):
+        if not _t.text:
+            return None
+        m = puncs.search(_t.text)
+        if not m:
+            return None
+        _tp = _t.getparent()
+        nwr = copy.deepcopy(_tp)
+        nwr2 = copy.deepcopy(_tp)
+        nwr.xpath('./w:t', namespaces=nss)[0].text = m.group(0)
+        nwrrpr = nwr.xpath('./w:rPr', namespaces=nss)
+        if len(nwrrpr) == 0:
+            nwrrpr = ET.Element('{%s}rPr' % nss['w'], nsmap=nss)
+            nwr.insert(0, nwrrpr)
+        else:
+            nwrrpr = nwrrpr[0]
+        nwrrpr.append(ET.Element('{%s}rFonts' % nss['w'], {
+            '{%s}asciiTheme'    % nss['w']:"majorEastAsia", 
+            '{%s}eastAsiaTheme' % nss['w']:"majorEastAsia", 
+            '{%s}hAnsiTheme'    % nss['w']:"majorEastAsia"}, nsmap=nss))
+        nwr2.xpath('./w:t', namespaces=nss)[0].text = _t.text[m.end():]
+        _t.text = _t.text[:m.start()]
+        _tpp = _tp.getparent()
+        _tpp.insert(_tpp.index(_tp)+1, nwr)
+        _tpp.insert(_tpp.index(_tp)+2, nwr2)
+        return nwr2.xpath('./w:t', namespaces=nss)[0]
+    
+    # deal with indent
+    for _p in xt.xpath('//w:p', namespaces=nss):
+        _deal_p(_p)
+    
+    # deal with quotation marks
+    puncs = re.compile(ur'[…·“”‘’]')
+    for __t in xt.xpath('//w:t', namespaces=nss):
+        _t = __t
+        while _t is not None:
+            _t = _deal_t(_t)
+    
+    doc = '<?xml version="1.0" encoding="UTF-8"?>' + ET.tostring(xt, encoding=ET.ElementTree(xt).docinfo.encoding)
+    #print doc; return
+    
+    zf['word/document.xml'] = doc
     
     z = zipfile.ZipFile(docx, 'w')
     for _ in zf:
